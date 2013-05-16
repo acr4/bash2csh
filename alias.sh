51b9d5a149f9febdf0a2bbb13267f398e8da3e09
Alex Reed <acreed4@gmail.com>
2013-05-16 17:23:10 -0400

    command --> \

--- alias.sh
+++ alias.sh
@@ -1,21 +1,23 @@
 # alias 'alias' to handle csh-style aliases
-function alias () {
+unset -f __alias
+function __alias () {
   #echo "function alias() called with ${#@} args:  ${@}"
   local E_BADARGS=65
   case ${#@} in
     0) # bareword 'alias', which means "print all aliases".  pipe through less for easier reading
-      command alias -p | less
+      \alias -p | less
       ;;
     1) # bash alias, or "alias -p"
-      command alias "${1}"
+      \alias "${1}"
       ;;
     2) # assume csh alias. could also be a valid "alias -p <name>" but that's unlikely.  don't even bother checking for it
-      command alias "${1}"="${2}"
+      \alias "${1}"="${2}"
       ;;
     *) # assume this to be a csh alias that lacks '' or "" around the RHS.
       local lhs=${1}
       shift
-      command alias "${lhs}"='${@}'
+      \alias "${lhs}"='${@}'
       ;;
   esac
 }
+alias alias=__alias
