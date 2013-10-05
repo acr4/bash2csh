e337e959e575f512dcdc83041ddec38c43faf4da
Alex Reed <acreed4@gmail.com>
2013-10-04 22:16:39 -0400

    Create csh-like setenv/unsetenv functions

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,21 @@
+# -*- mode:shell-script -*-
+
+
 # Map 'setenv' commands from csh to bash 'export'
+unset setenv
 function setenv () {
-  # play nice w/ .csh
   export ${1}=${2}
+}
+
+
+# Map 'unsetenv' commands from csh to bash 'export'
+# unsetenv supports globbing
+unset unsetenv
+function unsetenv () {
+  pattern="^${1/\*/.*}\$"
+  while read var
+  do
+    [[ ${var} =~ ${pattern} ]] && unset ${var}
+  done < <(env | \grep -Eo ^[^=]+)
+  :
 }
